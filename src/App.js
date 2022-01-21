38faa20dae68162d4e7b34c0ee2e7f03464ec1f8
Ethan Grissom <ethan.grissom@gmail.com>
2022-01-21 12:53:30 -0500

    Basic landing page + file drop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Paragraph, Anchor, Text, Grommet, Main, Header, FileInput, List } from 'grommet';
+import * as zip from '@zip.js/zip.js'
+import reactDom from 'react-dom';
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+    colors: {
+      header: '#6600cc',
+      landing: '#4d79ff',
+      link: 'white',
+    }
+  },
+};
+
+
+//zip.js Example
+async function parseZipContents(blob) {
+  // create a BlobReader to read with a ZipReader the zip from a Blob object
+  const reader = new zip.ZipReader(new zip.BlobReader(blob));
+
+  // get all entries from the zip
+  const entries = await reader.getEntries();
+  if (entries.length) {
+
+    // get first entry content as text by using a TextWriter
+    const text = await entries[0].getData(
+      // writer
+      new zip.TextWriter(),
+      // options
+      {
+        onprogress: (index, max) => {
+          // onprogress callback
+        }
+      }
+    );
+    // text contains the entry data as a String
+    return text;
+
+  }
+
+  // close the ZipReader
+  await reader.close();
+}
+
+class Table extends React.Component {
+  render() {
+    return (
+      <List
+        primaryKey="name"
+        secondaryKey="percent"
+        data={[
+          { name: 'Alan', percent: 20 },
+          { name: 'Bryan', percent: 30 },
+          { name: 'Chris', percent: 40 },
+          { name: 'Eric', percent: 80 },
+        ]}
+      />
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <Header background='header' pad='small'>
+        <Text size='medium'>🏃</Text>
+        <Anchor alignSelf='end' color='link' href="https://github.com/GrissomE">GrissomE</Anchor>
+      </Header>
+      <Main flex align='center' justify='center'>
+        <Paragraph >Upload your Strava history to generate view</Paragraph>
+        <FileInput name="Upload" multiple={false}
+          onChange={event => {
+            if (event.target.files) {
+              const fileList = event.target.files;
+              for (let i = 0; i < fileList.length; i += 1) {
+                const file = fileList[i];
+              }
+              parseZipContents(fileList[0]).then(val => {
+                reactDom.render(< Table contents={val} />, document.getElementById('table'))
+              });
+            }
+          }}
+        />
+        <div id="table"></div>
+      </Main>
+    </Grommet >
   );
 }
 
